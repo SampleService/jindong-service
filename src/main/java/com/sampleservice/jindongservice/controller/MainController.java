ce824ee445b069d5a80afc07460f7d77c3502cf1
SeptemberHX <september_hx@outlook.com>
2019-12-03 16:54:06 +0800

    f

--- src/main/java/com/sampleservice/jindongservice/controller/MainController.java
+++ src/main/java/com/sampleservice/jindongservice/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.sampleservice.jindongservice.controller;
 
+import com.sampleservice.jindongservice.utils.MBaseUtils;
 import com.septemberhx.common.base.MResponse;
 import com.septemberhx.mclient.annotation.MApiFunction;
 import com.septemberhx.mclient.annotation.MRestApiType;
@@ -20,6 +21,10 @@ public class MainController extends MObject {
     public MResponse deliveryFunction(@RequestBody MResponse requestData) {
         MResponse result = new MResponse();
         result.set("msg", "/delivery");
+
+        if (requestData.get("interval") != null) {
+            MBaseUtils.generateStringInKBSize(1000, result, (int) requestData.get("interval"));
+        }
         return result;
     }
 
@@ -30,6 +35,10 @@ public class MainController extends MObject {
     public MResponse marketFunction(@RequestBody MResponse requestData) {
         MResponse result = new MResponse();
         result.set("msg", "/market");
+
+        if (requestData.get("interval") != null) {
+            MBaseUtils.generateStringInKBSize(1000, result, (int) requestData.get("interval"));
+        }
         return result;
     }
 }
